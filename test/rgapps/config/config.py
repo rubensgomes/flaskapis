c0d7c55c80b674dd74a8178ef8b8ce9c38030421
Rubens Gomes <rubens.s.gomes@gmail.com>
2015-07-18 15:29:03 -0400

    refactoring

---
+++ test/rgapps/config/config.py
@@ -0,0 +1,38 @@
+"""test.rgapps.config.config module
+
+Unit test for rgapps.config.config module
+"""
+import os
+import unittest
+import logging
+
+from rgapps.config.config import initialize_environment
+
+
+__author__ = "Rubens S. Gomes <rubens.s.gomes@gmail.com>"
+__copyright__ = "Copyright (c) 2015 Rubens S. Gomes"
+__license__ = "All Rights Reserved"
+__maintainer__ = "Rubens Gomes"
+__email__ = "rubens.s.gomes@gmail.com"
+__status__ = "Experimental"
+
+LOG_FILE_PATH = r"C:\personal\flaskapis\testing.log"
+
+class ConfigTestCase(unittest.TestCase):
+
+    def setUp(self):
+        initialize_environment(log_file_path=LOG_FILE_PATH)
+        return
+
+    def tearDown(self):
+        handlers = logging.getLogger().handlers[:]
+        for handler in handlers:
+            handler.close()
+            logging.getLogger().removeHandler(handler)
+        if os.path.isfile(LOG_FILE_PATH):
+            os.remove(LOG_FILE_PATH)
+
+    def test_logging(self):
+        logging.debug("testing logging")
+        return
+

b7840cda0a449e36279733dcfdca5ff35a8be021
Rubens Gomes <rubens.s.gomes@gmail.com>
2015-07-18 23:30:50 -0400

    refactoring

---
+++ rgapps/domain/__init__.py
@@ -0,0 +1,26 @@
+"""rgapps.domain package
+
+This is the top root package for all the domain code.
+
+Modules:
+-------
+email.py: Email related code.
+sms.py: SMS related code
+
+Sub-Packages:
+------------
+
+
+"""
+
+__author__ = "Rubens S. Gomes <rubens.s.gomes@gmail.com>"
+__copyright__ = "Copyright (c) 2015 Rubens S. Gomes"
+__license__ = "All Rights Reserved"
+__maintainer__ = "Rubens Gomes"
+__email__ = "rubens.s.gomes@gmail.com"
+__status__ = "Experimental"
+
+__all__ = ["email", "sms"]
+
+if __name__ == '__main__':
+    pass

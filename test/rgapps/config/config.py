aa391b2a8d9e964dfb22783dcd6c73d9b7d5b9fd
Rubens Gomes <rubens.s.gomes@gmail.com>
2015-07-25 19:17:59 -0400

    refactoring

--- test/rgapps/config/config.py
+++ test/rgapps/config/config.py
@@ -18,21 +18,22 @@ __status__ = "Experimental"
 
 LOG_FILE_PATH = r"C:\personal\flaskapis\testing.log"
 
-class ConfigTestCase(unittest.TestCase):
+class ConfigTestCase( unittest.TestCase ):
 
-    def setUp(self):
-        initialize_environment(log_file_path=LOG_FILE_PATH)
+    def setUp( self ):
+        initialize_environment( log_file_path=LOG_FILE_PATH )
         return
 
-    def tearDown(self):
+    def tearDown( self ):
         handlers = logging.getLogger().handlers[:]
         for handler in handlers:
             handler.close()
-            logging.getLogger().removeHandler(handler)
-        if os.path.isfile(LOG_FILE_PATH):
-            os.remove(LOG_FILE_PATH)
+            logging.getLogger().removeHandler( handler )
+        if os.path.isfile( LOG_FILE_PATH ):
+            os.remove( LOG_FILE_PATH )
+        return
 
-    def test_logging(self):
-        logging.debug("testing logging")
+    def test_logging( self ):
+        logging.debug( "testing logging" )
         return
 

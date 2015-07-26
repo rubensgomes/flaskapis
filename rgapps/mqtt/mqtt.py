a5c4911847f815641c63bd19559ef61b824936e5
Rubens Gomes <rubens.s.gomes@gmail.com>
2015-07-26 14:03:38 -0400

    refactoring

--- rgapps/mqtt/mqtt.py
+++ rgapps/mqtt/mqtt.py
@@ -6,12 +6,13 @@ from collections import OrderedDict
 import json
 import logging
 
-from flask import current_app
 from paho.mqtt import publish
-from werkzeug.exceptions import BadRequest
 
 import paho.mqtt.client as mqtt
-from rgapps.domain.sensor import SensorTemperature
+from rgapps.config import ini_config
+from rgapps.domain.ds18b20sensor import DS18B20Sensor
+from rgapps.utils.exception import IllegalArgumentException
+from rgapps.utils.utility import is_blank
 
 
 __author__ = "Rubens S. Gomes <rubens.s.gomes@gmail.com>"
@@ -29,7 +30,7 @@ class MQTTPublisher():
     """MQTT message publisher.
     """
 
-    def publish_temperature(self, serial):
+    def publish_temperature( self, serial ):
         """
         Publishes the given temperature sensor data to MQTT
         message broker.
@@ -45,43 +46,49 @@ class MQTTPublisher():
         """
 
         # test serial
-        if not serial:
-            raise BadRequest("serial is missing")
+        if is_blank( serial ):
+            raise IllegalArgumentException( "serial is required" )
 
-        mqtt_id = current_app.config['MQTT_CLIENT_ID']
-        user = current_app.config['MQTT_USERNAME']
-        pw = current_app.config['MQTT_PASSWORD']
-        host = current_app.config['MQTT_HOST']
-        port = current_app.config['MQTT_PORT']
-        topic = current_app.config['MQTT_TOPIC']
+        mqtt_id = ini_config.get( "MQTT", "MQTT_CLIENT_ID" )
+        user = ini_config.get( "MQTT", "MQTT_USERNAME" )
+        pw = ini_config.get( "MQTT", "MQTT_PASSWORD" )
+        host = ini_config.get( "MQTT", "MQTT_HOST" )
+        port = ini_config.get( "MQTT", "MQTT_PORT" )
+        topic = ini_config.get( "MQTT", "MQTT_TOPIC" )
 
 
-        mqttc = mqtt.Client(client_id=mqtt_id, clean_session=True,
-                            protocol=mqtt.MQTTv31)
-        mqttc.username_pw_set(user, password=pw)
+        mqttc = mqtt.Client( client_id=mqtt_id, clean_session=True,
+                            protocol=mqtt.MQTTv31 )
+        mqttc.username_pw_set( user, password=pw )
 
-        logging.debug("Connecting to MQTT Broker: "
+        logging.debug( "Connecting to MQTT Broker: "
                       "host [{0}], port [{1}], client id [{2}], "
                       "user [{3}], sensor serial [{4}]"
-                      .format(host, port, mqtt_id, user, serial))
+                      .format( host, port, mqtt_id, user, serial ) )
 
-        sensor_temperature = SensorTemperature()
-        message = sensor_temperature.get(serial)
-        json_message = json.dumps(message, indent=2, sort_keys=True)
+        sensor_temperature = DS18B20Sensor( serial )
+        readings = sensor_temperature.get_measurement()
+
+        message = OrderedDict()
+        message["value"] = readings.get_value()
+        message["unit"] = readings.get_unit()
+        message["utc"] = readings.get_utc()
+
+        json_message = json.dumps( message, indent=2, sort_keys=True )
 
         auth = OrderedDict()
         auth["username"] = user
         auth["password"] = pw
 
-        publish.single(topic, payload=json_message, qos=0,
+        publish.single( topic, payload=json_message, qos=0,
                        retain=False, hostname=host,
                        port=port, client_id=mqtt_id,
-                       keepalive=20, auth=auth)
+                       keepalive=20, auth=auth )
 
-        logging.debug("Message was published correctly: "
-                      "host [{0}], port [{1}], client id [{2}], "
-                      "user [{3}], sensor serial [{4}]"
-                      .format(host, port, mqtt_id, user, serial))
+        logging.debug( "Message [{0}] was published correctly: "
+                      "host [{1}], port [{2}], client id [{3}], "
+                      "user [{4}], sensor serial [{5}]"
+                      .format( message, host, port, mqtt_id, user, serial ) )
 
         return
 

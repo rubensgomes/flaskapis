1ca2075d332868aedfd481f30b5241ed9bbcdcee
Rubens Gomes <rubens.gomes@aa.com>
2022-06-26 02:31:32 +0100

    updates

--- playground/toh.py
+++ playground/toh.py
@@ -6,35 +6,43 @@ Created on Feb 11, 2022
 
 
 def toh(n, fromStack, toStack, spareStack):
-
+    """
+    Inputs: n, positive integer containing number of disks
+            fromStack, array containing disks to be moved from
+            toStack, array to where disks should be moved to
+            spareStack, array to keep temporary spare disks during moves
+    Returns None
+    """
+    # recursive base case
     if n == 1:
         toStack.append(fromStack.pop())
-        print ("frontStack: ", fromStack)
+        print ("fromStack: ", fromStack)
         print ("toStack: ", toStack)
         print ("spareStack: ", spareStack)
         print("-------")
-        return
+        return None
 
+    # notice these are recursive calls
     toh(n - 1, fromStack, spareStack, toStack)
     toh(1, fromStack, toStack, spareStack)
     toh(n - 1, spareStack, toStack, fromStack)
 
-    return
+    return None
 
 
 if __name__ == '__main__':
     print('toh starting...')
-    fromStack = ["0", "1", "2", "3"]
-    toStack = []
-    spareStack = []
+    _fromStack = ["0", "1", "2", "3"]
+    _toStack = []
+    _spareStack = []
 
-    print ("initial frontStack: ", fromStack)
-    print ("initial toStack: ", toStack)
-    print ("initial spareStack: ", spareStack)
+    print ("initial fromStack: ", _fromStack)
+    print ("initial toStack: ", _toStack)
+    print ("initial spareStack: ", _spareStack)
     print("-------")
 
-    toh(len(fromStack), fromStack, toStack, spareStack)
+    toh(len(_fromStack), _fromStack, _toStack, _spareStack)
 
-    print ("final frontStack: ", fromStack)
-    print ("final toStack: ", toStack)
-    print ("final spareStack: ", spareStack)
+    print ("final fromStack: ", _fromStack)
+    print ("final toStack: ", _toStack)
+    print ("final spareStack: ", _spareStack)
